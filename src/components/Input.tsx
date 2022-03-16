0bac2b8ecf41e97e5bbea6ccf391a51fa2653bdb
Daniel <andriaritiana@yahoo.fr>
2022-03-16 15:37:49 +0300

    fix(input): could not deploy to npm

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export interface InputProps {
-  fullWidth?: boolean
+  fullWidth?: boolean;
+  hasError?: boolean; 
 }
 
 export const Input = styled.input<InputProps>`
@@ -12,6 +13,6 @@ width: ${props => props.fullWidth ? '100%' : 'auto'};
 font-size: 1em;
 margin: 1em;
 padding: 0.25em 1em;
-border: 2px solid #2196f3;
+border: 2px solid ${props => props.hasError ? 'red' : '#2196f3'};
 border-radius: 3px;
 `;
