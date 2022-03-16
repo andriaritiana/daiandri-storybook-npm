10af88a8f2b9a748221eefe289a77c5f37be8ba0
Daniel <andriaritiana@yahoo.fr>
2022-03-16 12:16:08 +0300

    added input component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import { action } from '@storybook/addon-actions';
 import { Button } from "../components/Button";
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
   component: Button,
 };
 
